21f9e175a59224513db749fbadc0fa2ab2ddfa3b
siwaniagrawal <siwaniagrawal18@gmail.com>
2019-03-24 13:05:59 +0530

    added support for land_type chips suggestion

--- functions/land.js
+++ functions/land.js
@@ -1,12 +1,33 @@
 var config = require('./config')
 const requestLib = require('request');
 const utils = require('./utils');
+const path=require('path');
+
+const fs = require('fs');
 
 exports.processRequest = function(conv, parameters) {
     return new Promise(function(resolve, reject) {
-        if (parameters.land_type !== "" && parameters.land_region !== "") {
-            let land_type = parameters.land_type;
+        if (parameters.land_region !== "") {
             let land_region = parameters.land_region;
+            let land_type = parameters.land_type;
+            let land_types = []
+            if (parameters.land_type === ""){
+             
+                fs.readFile(path.join(__dirname, './assets/raw_data_land_chips.json'), function(err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log("data: ", JSON.parse(data));
+                    data = JSON.parse(data);
+                    data.forEach((obj) => {
+                        console.log("land", obj.land);
+                        land_types.push(obj.land);
+                    });
+                    utils.responseWithSuggestions(conv, "Please select from the following types of lands", land_types);
+                    resolve();
+                    return;
+                });
+            }
             var options = {
                 uri: config.endpoint + "/land",
                 method: 'POST',
@@ -52,6 +73,4 @@ exports.processRequest = function(conv, parameters) {
             resolve();
         }
     });
-    
 }
-
